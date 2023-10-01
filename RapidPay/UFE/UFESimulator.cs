981d9a85f3dfed040026506fb9268fb2285b76a1
Alexsandro Oliveira <alexsandroaoliveira@gmail.com>
2023-10-01 14:54:26 -0300

    refact: Improving UFE Sim

--- RapidPay/UFE/UFESimulator.cs
+++ RapidPay/UFE/UFESimulator.cs
@@ -4,7 +4,7 @@ public class UFESimulator : IUFEClient
 {
     private readonly ICurrentDateProvider _currentDateProvider;
     private decimal fee = -1;
-    private DateTime feeCreatedDate = DateTime.Now;
+    private DateTime feeCreatedDate = new(1990, 1, 1);
 
     public UFESimulator(ICurrentDateProvider currentDateProvider)
     {
@@ -14,7 +14,7 @@ public class UFESimulator : IUFEClient
     public decimal GetCurrentFee()
     {
         var currentDate = _currentDateProvider.GetCurrentDate();
-        if (currentDate.Day != feeCreatedDate.Day || currentDate.Hour != feeCreatedDate.Hour || fee == -1)
+        if (currentDate.Day != feeCreatedDate.Day || currentDate.Hour != feeCreatedDate.Hour)
         {
             fee = (decimal)new Random().Next(200) / 100;
             feeCreatedDate = currentDate;
