739312f2ced63864eb51008bbbc8e46901b1b054
Alexsandro Oliveira <alexsandroaoliveira@gmail.com>
2023-10-01 13:26:59 -0300

    feat: InMemory Data Repository

---
+++ RapidPay/Data/RapidPayRepository.cs
@@ -0,0 +1,11 @@
+﻿using RapidPay.Models;
+using System.Collections.Concurrent;
+
+namespace RapidPay.Data
+{
+    public class RapidPayRepository
+    {
+        public ConcurrentBag<Card> Cards { get; set; } = new();
+        public ConcurrentBag<Transaction> Transactions { get; set; } = new();
+    }
+}
