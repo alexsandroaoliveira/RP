6912b2ca94c160a791dee333578ab6a60d815d44
Alexsandro Oliveira <alexsandroaoliveira@gmail.com>
2023-10-01 14:16:30 -0300

    feat: RapidPayContext

---
+++ RapidPay/RapidPayContext.cs
@@ -0,0 +1,26 @@
+﻿namespace RapidPay
+{
+    public static class RapidPayContext
+    {
+        private static readonly object LastFeeLocker = new();
+        private static decimal _lastFee = 1;
+
+        public static decimal LastFee
+        {
+            get
+            {
+                lock (LastFeeLocker)
+                {
+                    return _lastFee;
+                }
+            }
+            set
+            {
+                lock (LastFeeLocker)
+                {
+                    _lastFee = value;
+                }
+            }
+        }
+    }
+}
